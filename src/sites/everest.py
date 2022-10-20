3d80c731cea1bb542cd2a62702dcc15eebe8412c
Nino Verde <nino.verde@leonardocompany.com>
2022-10-19 23:52:49 -0700

    Everest DLS updated

--- src/sites/everest.py
+++ src/sites/everest.py
@@ -14,24 +14,28 @@ class Everest(SiteCrawler):
     def _handle_page(self, body: str):
         soup = BeautifulSoup(body, "html.parser")
 
-        victim_list = soup.find_all("header", class_="entry-header has-text-align-center")
+        victim_list = soup.find_all(
+            "header", class_="entry-header has-text-align-center")
 
         for victim in victim_list:
-            victim_name = victim.find("h2", class_="entry-title heading-size-1").text.strip()
+            victim_name = victim.find(
+                "h2", class_="entry-title heading-size-1").text.strip()
 
-            victim_leak_site = victim.find("h2", class_="entry-title heading-size-1").find("a").attrs["href"]
+            victim_leak_site = victim.find(
+                "h2", class_="entry-title heading-size-1").find("a").attrs["href"]
 
-            published = victim.find("li", class_="post-date meta-wrapper").find("a").text.strip()
-            published_dt = datetime.strptime(
-                published, "%B %d, %Y")
-            
+            #published = victim.find("li", class_="post-date meta-wrapper").find("a").text.strip()
+            # published_dt = datetime.strptime(
+            #    published, "%B %d, %Y")
+            published_dt = datetime.now()
+
             q = self.session.query(Victim).filter_by(
                 url=victim_leak_site, site=self.site)
 
             if q.count() == 0:
                 # new victim
                 v = Victim(name=victim_name, url=victim_leak_site, published=published_dt,
-                            first_seen=datetime.utcnow(), last_seen=datetime.utcnow(), site=self.site)
+                           first_seen=datetime.utcnow(), last_seen=datetime.utcnow(), site=self.site)
                 self.session.add(v)
                 self.new_victims.append(v)
             else:
@@ -43,7 +47,6 @@ class Everest(SiteCrawler):
             self.current_victims.append(v)
         self.session.commit()
 
-
     def scrape_victims(self):
         with Proxy() as p:
             r = p.get(f"{self.url}", headers=self.headers)
@@ -52,15 +55,43 @@ class Everest(SiteCrawler):
             # find all pages
             page_nav = soup.find_all("a", class_="page-numbers")
 
-            
             site_list = []
             site_list.append(self.url)
-            
+
             for page in page_nav:
                 # might exist repetition
                 if page.attrs["href"] not in site_list:
                     site_list.append(page.attrs["href"])
-            
+            max_page = 0
+            min_page = 1000
+
             for site in site_list:
-                r = p.get(site, headers=self.headers)
-                self._handle_page(r.content.decode()) 
+                site_splitted = site.split("/")
+                #print("Site_splitted: ")
+                # print(site_splitted)
+                if (len(site_splitted) >= 4 and site_splitted[4].isdigit() and max_page < int(site_splitted[4])):
+                    max_page = int(site_splitted[4])
+                if (len(site_splitted) >= 4 and site_splitted[4].isdigit() and min_page > int(site_splitted[4])):
+                    min_page = int(site_splitted[4])
+            #print("Max Page: "+str(max_page))
+            #print("Min Page: "+str(min_page))
+
+            base_site = site_list[0]
+            r = p.get(base_site, headers=self.headers)
+            self._handle_page(r.content.decode())
+
+            for pagenum in range(min_page, max_page+1):
+                site_tovisit = base_site + \
+                    "/page/"+str(pagenum)+"/"
+                #print("Site "+site_tovisit)
+                r = p.get(site_tovisit, headers=self.headers)
+                self._handle_page(r.content.decode())
+
+            self.site.last_scraped = datetime.utcnow()
+            self.session.commit()
+            # for site in site_list:
+            #    if (site.startswith("//")):
+            #        site = "https:"+site
+            #    print("Site "+site)
+            #    r = p.get(site, headers=self.headers)
+            #    self._handle_page(r.content.decode())
