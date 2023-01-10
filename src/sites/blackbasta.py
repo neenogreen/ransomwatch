a6acbeef8e0b417e894498684073a094d26c426e
IOC <ioc@local.it>
2023-01-10 10:22:47 +0100

    Migrate blackbasta dls from websocket to headless browser

--- src/sites/blackbasta.py
+++ src/sites/blackbasta.py
@@ -4,82 +4,77 @@ from config import Config
 from random import randint
 from time import sleep
 
-import json
-import websocket
+from bs4 import BeautifulSoup
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 
 from db.models import Victim
 from net.proxy import Proxy
-from notifications.manager import NotificationManager
+from net.headless_browser import HeadlessBrowser
 from .sitecrawler import SiteCrawler
+from notifications.manager import NotificationManager
 
 class Blackbasta(SiteCrawler):
     actor = "Blackbasta"
 
+    def __init__(self, url: str):
+        super(Blackbasta, self).__init__(url)
+
     def is_site_up(self) -> bool:
-        ws = websocket.WebSocket()
-        try:
-            ws.connect(f"ws{self.url[4:]}/ws",
-                      http_proxy_host=Config["proxy"]["hostname"],
-                      http_proxy_port=Config["proxy"]["socks_port"],
-                      proxy_type="socks5h")
-            ws.send("{\"name\":\"get_companies\"}")
-            res = ws.recv()
-            ws.close()
-        except Exception:
-            return False
-        if res:
-            self.site.last_up = datetime.utcnow()
-            return True
-        return False
-        
-    def _handle_page(self, body: str):
-        victim_list = json.loads(body)
-        victim_list = victim_list["additional"]["Companies"]
+        with Proxy() as p:
+            try:
+                r = p.get(self.url, headers=self.headers)
 
-        for victim in victim_list:
-            victim_name = victim["title"]
-            pub_data = victim["data_published"]
-            victim_leak_site = self.url + "/?id=" + victim_name + "#" + str(pub_data)
-            description = victim["description"]
-            
-            q = self.session.query(Victim).filter_by(
-                name=victim_name, site=self.site)
+                if r.status_code >= 400:
+                    return False
+            except Exception as e:
+                print(e)
+                return False
 
-            if q.count() == 0:
-                # new victim
-                v = Victim(name=victim_name, url=victim_leak_site,
-                            description=description,
-                            published=datetime.utcnow(),
-                            first_seen=datetime.utcnow(),
-                            last_seen=datetime.utcnow(), site=self.site)
-                self.session.add(v)
-                self.new_victims.append(v)
-            else:
-                # already seen, update last_seen
-                v = q.first()
-                old_pub = int(v.url.split("#")[-1])
-                v.last_seen = datetime.utcnow()
-                v.url = victim_leak_site
-                if pub_data != old_pub:
-                    NotificationManager.send_new_victim_notification(v)
-            # add the org to our seen list
-            self.current_victims.append(v)
+        self.site.last_up = datetime.utcnow()
+
+        return True
+
+    def _handle_page(self, browser):
+        while True:
+            soup = BeautifulSoup(browser.res(), "html.parser")
+
+            victim_list = soup.find_all("div", class_=['card'])
+
+            for victim in victim_list:
+                tmp = victim.find("a", class_="blog_name_link")
+                victim_name = tmp.text.strip()
+                victim_leak_site = tmp.href
+                
+                q = self.session.query(Victim).filter_by(
+                    url=victim_leak_site, site=self.site)
+
+                if q.count() == 0:
+                    # new victim
+                    description = soup.find("div", class_="vuepress-markdown-body").get_text().strip()
+                    v = Victim(name=victim_name, description=description, url=victim_leak_site, published=datetime.utcnow(), first_seen=datetime.utcnow(), last_seen=datetime.utcnow(), site=self.site)
+                    self.session.add(v)
+                    self.new_victims.append(v)
+                else:
+                    # already seen, update last_seen
+                    v = q.first()
+                    v.last_seen = datetime.utcnow()
+
+                # add the org to our seen list
+                self.current_victims.append(v)
+            try:
+                browser.DRIVER.find_element(By.XPATH, '//div[@class="next-page-btn"]').click()
+            except NoSuchElementException:
+                break
+
+            sleep(10)
+
         self.session.commit()
 
     def scrape_victims(self):
-        ws = websocket.WebSocket()
-        ws.connect(f"ws{self.url[4:]}/ws",
-                  http_proxy_host=Config["proxy"]["hostname"],
-                  http_proxy_port=Config["proxy"]["socks_port"],
-                  proxy_type="socks5h")
-        ws.send("{\"name\":\"get_companies\"}")
-        ws.recv()
-        cnt = json.loads(ws.recv())
-        cnt = cnt["additional"]["Pages"]
-
-        for i in range(1, cnt+1):
-            ws.send("{\"name\":\"get_companies\",\"additional\":"+str(i)+"}")
-            self._handle_page(ws.recv())
-
-        ws.close()
+        with HeadlessBrowser() as browser:
+            browser.get(f"{self.url}")
+            sleep(15)
+            self._handle_page(browser)
         self.site.last_scraped = datetime.utcnow()
+
