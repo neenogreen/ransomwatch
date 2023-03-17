fa515bb0e8e46016d7a726995f569fd6f470e4a2
IOC <ioc@local.it>
2023-03-17 11:34:46 +0100

    Fix Cl0p

--- src/sites/cl0p.py
+++ src/sites/cl0p.py
@@ -19,15 +19,14 @@ class Cl0p(SiteCrawler):
 
             # get max page number
             victim_list = soup.find("ul", class_="g-toplevel").find_all("li", class_="g-menu-item")
+            victim_list.extend(soup.find("ul", class_="g-toplevel").find("li", class_="g-menu-item-archive").find("ul", class_="g-sublevel").find_all("li")[1:])
+            victim_list.extend(soup.find("ul", class_="g-toplevel").find("li", class_="g-menu-item-archive2").find("ul", class_="g-sublevel").find_all("li")[1:])
+            victim_list.extend(soup.find("ul", class_="g-toplevel").find("li", class_="g-menu-item-archive3").find("ul", class_="g-sublevel").find_all("li")[1:])
             for victim in victim_list:
                 victim_name = victim.find("span", class_="g-menu-item-title").text.strip()
-                
-                if victim_name in ("HOME", "HOW TO DOWNLOAD?", "ARCHIVE"):
+                if victim_name in ("HOME", "HOW TO DOWNLOAD?", "ARCHIVE", "ARCHIVE2", "ARCHIVE3"):
                     continue
-                
                 victim_leak_site = self.url + victim.find("a").attrs["href"]
-                
-                
                 q = self.session.query(Victim).filter_by(
                     url=victim_leak_site, site=self.site)
 
@@ -35,7 +34,10 @@ class Cl0p(SiteCrawler):
                     # new victim
                     r = p.get(victim_leak_site, headers=self.headers)
                     soup1 = BeautifulSoup(r.content.decode(), "html.parser")
+
                     description = soup1.find("p").text.strip()
+                    if "Due to the fact that the Tor network is abandoning the second version and all domains will be abolished in September or October, we are moving to a new address" in description:
+                        description = soup1.find_all("p")[1].text.strip()
                     
                     v = Victim(name=victim_name, url=victim_leak_site,
                             description=description,
