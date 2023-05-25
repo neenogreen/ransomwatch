0c7ff476eec592ec1ddab654c7ef27bae89e6235
IOC <ioc@local.it>
2023-05-25 12:23:39 +0200

    Update Nokoyawa dls

--- src/sites/nokoyawa.py
+++ src/sites/nokoyawa.py
@@ -2,6 +2,7 @@ from datetime import datetime
 import logging
 import re
 from bs4 import BeautifulSoup
+import urllib.parse
 
 from db.models import Victim
 from net.proxy import Proxy
@@ -10,32 +11,33 @@ from .sitecrawler import SiteCrawler
 class Nokoyawa(SiteCrawler):
     actor = "Nokoyawa"
 
+    def is_site_up(self) -> bool:
+        with Proxy() as p:
+            try:
+                r = p.get(self.url + "/api/leaks/get")
+                if r.status_code >= 400:
+                    return False
+            except Exception as e:
+                return False
+        self.site.last_up = datetime.utcnow()
+        return True
+
     def scrape_victims(self):
         with Proxy() as p:
-            r = p.get(f"{self.url}", headers=self.headers)
-        soup = BeautifulSoup(r.content.decode(), "html.parser")
-        victim_list = soup.find_all("div", {'id': re.compile(r'overlay')})
+            r = p.get(f"{self.url}/api/leaks/get", headers=self.headers).json()["payload"]
 
-        for victim in victim_list:
-            tmp = victim.find("h3")
-            published_str = tmp.find("span").text.strip()
-            try:
-                published = datetime.strptime(published_str, "(%b. %d, %Y, ")
-            except ValueError as v:
-                if len(v.args) > 0 and v.args[0].startswith('unconverted data remains: '):
-                    line = published_str[:-(len(v.args[0]) - 26)]
-                    published = datetime.strptime(line, "(%b. %d, %Y, ")
-                else:
-                    raise
-            victim_name = tmp.text.strip().replace(published_str, "").replace("\n", "").strip()
+        for victim in r:
+            published = datetime.strptime(victim["createdAt"].split(".")[0], "%Y-%m-%dT%H:%M:%S")
+            description = victim["description"]
+            victim_url = self.url + "/leak/" + victim["_id"]
+            victim_name = urllib.parse.unquote(victim["title"])
             
             q = self.session.query(Victim).filter_by(
-                name=victim_name, site=self.site)
+                name=victim_name, url=victim_url, site=self.site)
 
             if q.count() == 0:
                 # new victim
-                description = victim.find("p").text.strip()
-                v = Victim(name=victim_name, published=published, description=description, first_seen=datetime.utcnow(), last_seen=datetime.utcnow(), site=self.site)
+                v = Victim(name=victim_name, published=published, description=description, first_seen=datetime.utcnow(), last_seen=datetime.utcnow(), site=self.site, url=victim_url)
                 self.session.add(v)
                 self.new_victims.append(v)
             else:
