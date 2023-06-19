46a06a06d7b837a3bd3595ef72c6ac7efa33c848
IOC <ioc@local.it>
2023-06-19 10:12:20 +0200

    Fix trigona

--- src/sites/trigona.py
+++ src/sites/trigona.py
@@ -1,7 +1,6 @@
 from datetime import datetime
 import logging
-import re
-from bs4 import BeautifulSoup
+import html2text
 
 from db.models import Victim
 from net.proxy import Proxy
@@ -11,25 +10,20 @@ class Trigona(SiteCrawler):
     actor = "Trigona"
 
     def _handle_page(self, body: str):
-        soup = BeautifulSoup(body, "html.parser")
-        victim_list = soup.find("div", class_="grid").find_all("a")
+        victim_list = body["data"]["leaks"]
 
         for victim in victim_list:
-            victim_name = victim.find("div", class_="grid-caption__title").text.strip()
-            victim_leak_site = self.url + victim["href"]
+            victim_name = victim["title"].strip()
+            victim_leak_site = victim["link"].strip()
 
             q = self.session.query(Victim).filter_by(
                 url=victim_leak_site, site=self.site)
 
             if q.count() == 0:
                 # new victim
-                with Proxy() as p:
-                    r = p.get(victim_leak_site, headers=self.headers)
-                try:
-                    description = BeautifulSoup(r.content.decode(), "html.parser").find("div", class_="auction-item-info__text").get_text().strip()
-                except:
-                    description = ""
-                v = Victim(name=victim_name, url=victim_leak_site, published=datetime.utcnow(), description=description,
+                description = html2text.html2text(victim["descryption"])
+                published = datetime.strptime(victim["created_at"].split(".")[0], "%Y-%m-%dT%H:%M:%S")
+                v = Victim(name=victim_name, url=victim_leak_site, published=published, description=description,
                             first_seen=datetime.utcnow(), last_seen=datetime.utcnow(), site=self.site)
                 self.session.add(v)
                 self.new_victims.append(v)
@@ -45,6 +39,10 @@ class Trigona(SiteCrawler):
 
     def scrape_victims(self):
         with Proxy() as p:
-            r = p.get(self.url, headers=self.headers)
-            self._handle_page(r.content.decode()) 
+            i = 1
+            while True:
+                r = p.get(f"{self.url}/api?page={i}", headers=self.headers).json()
+                if not r["data"]["leaks"]: break
+                self._handle_page(r)
+                i = i + 1
         self.site.last_scraped = datetime.utcnow()
