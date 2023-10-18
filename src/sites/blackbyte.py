5c9f4be1f7b345a535f13bca2ef5454f7f085a2b
IOC <ioc@local.it>
2023-10-18 10:50:25 +0200

    Fix blackbyte

--- src/sites/blackbyte.py
+++ src/sites/blackbyte.py
@@ -2,21 +2,24 @@ from datetime import datetime
 import logging
 
 from bs4 import BeautifulSoup
+from time import sleep
 
 from db.models import Victim
 from net.proxy import Proxy
+from net.headless_browser import HeadlessBrowser
 from .sitecrawler import SiteCrawler
 
 
 class Blackbyte(SiteCrawler):
     actor = "Blackbyte"
 
-    def _handle_page(self, body: str):
-        soup = BeautifulSoup(body, "html.parser")
+    def _handle_page(self, browser):
+        soup = BeautifulSoup(browser.res(), "html.parser")
 
         victim_list = soup.select('div[class*="col-sm-12"]')
 
         for victim in victim_list:
+            if "If you are interested to purchase the data" in str(victim): continue
             victim_name = victim.find("h1").text.strip()
             description = victim.find("p").text.strip()
 
@@ -43,6 +46,7 @@ class Blackbyte(SiteCrawler):
 
 
     def scrape_victims(self):
-        with Proxy() as p:
-            r = p.get(f"{self.url}", headers=self.headers)
-            self._handle_page(r.content.decode())
+        with HeadlessBrowser() as browser:
+            browser.get(f"{self.url}")
+            sleep(15) 
+            self._handle_page(browser)
